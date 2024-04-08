d49538b7a0b181d39132d3538dd175b5f3a63f19
timohoo <timo.hoo55.th@gmail.com>
2024-04-08 19:45:09 +0300

    docs folder failed - emptied

--- docs/_nuxt/gwXqhpx9.js
+++
@@ -1,1 +0,0 @@
-import{i as t,J as l,aL as m,aM as r,h as e,o as p,D as u,R as c,y as d,a2 as i,B as f}from"./CWvnpu3D.js";const k=t({__name:"Modals.client",setup(_){const o=l(m),{isOpen:a}=r();return(y,n)=>e(o)?(p(),u(i(e(o).component),c({key:0},e(o).props,{modelValue:e(a),"onUpdate:modelValue":n[0]||(n[0]=s=>d(a)?a.value=s:null)}),null,16,["modelValue"])):f("",!0)}});export{k as default};
